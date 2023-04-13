e4a53a085cff8493533d8d9b0b40ad0892b922d3
PA <pierreteychene@gmail.com>
2023-04-13 16:03:48 +0200

    "creation des pages du site "

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,18 @@
 
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Home from './pages/Home/Home.jsx';
+import About from './pages/About/About.jsx';
+import FicheLogement from './pages/FicheLogement/FicheLogement.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logements/:id" element={<FicheLogement />} />
+      </Routes>
     </div>
   );
 }
---
+++ my-app/src/pages/CardLogement/CardLogement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FicheLogement() {
+  return (
+    <div>
+      <h1>voici la fiche Logement</h1>
+    </div>
+  )
+}
