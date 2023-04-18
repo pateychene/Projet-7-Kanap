31eae70d3d4374880763f920e6a12c9b0025f478
PA <pierreteychene@gmail.com>
2023-04-18 10:51:38 +0200

    affichage images logements , changement en sass footer

--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-function Card({title, image, id}){
+function Card({title, image , id}){
 
   return (
+    
      <Link to={`/fichelogement/${id}`}> 
+     <img src={image} alt="location" />
      <h3>{title}</h3>
      </Link>
   )
--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -1,5 +1,4 @@
 ///import React from 'react'
-import './Footer.css'
 import logoFooter from '../../assets/images/logofooter.svg'
 
 
