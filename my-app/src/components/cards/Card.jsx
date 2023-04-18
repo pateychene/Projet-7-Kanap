707fc93c2f059c7d4c04eaa74c4e48c8672b06f1
PA <pierreteychene@gmail.com>
2023-04-18 17:21:51 +0200

    affichage card ok

--- my-app/src/components/cards/Card.jsx
+++ my-app/src/components/cards/Card.jsx
@@ -1,10 +1,13 @@
 export default function Card({ cover, title }) { //les cards de locations récupèrent les couvertures et titres en props depuis Home
 	return (
-		<article className="card-logement">
+		<div className="cards-container">
+			<article className="card-logement">
 			<img src={cover} alt="location" />
 			<div className="card-logement__layer">
 				<p className="card-logement__title">{title}</p>
 			</div>
 		</article>
+		</div>
+		
 	);
 }
