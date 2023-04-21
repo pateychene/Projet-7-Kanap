5767b19c0340c1e9be5e86ab83fd271e3d2417f9
PA <pierreteychene@gmail.com>
2023-04-21 11:42:40 +0200

    footer affiche

--- my-app/src/components/footer/Footer.jsx
+++ my-app/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import logoFooter from '../../assets/images/logofooter.svg'
 
 function Footer(){
   return (
-      <footer>
+      <footer className='foot-container'>
       <img src={logoFooter} alt="logo de l'agence kaza"></img>
       <h2> ©2020 Kasa. All right reserved.</h2>
       </footer>
