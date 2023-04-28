6945bebfb04c71a6a0303b255b7c3fa800d1dfef
PA <pierreteychene@gmail.com>
2023-04-28 13:08:40 +0200

    finir page FicheLogement

--- my-app/src/pages/FicheLogement/FicheLogement.jsx
+++ my-app/src/pages/FicheLogement/FicheLogement.jsx
@@ -1,14 +1,17 @@
 import {useParams}  from 'react-router-dom'
 
 
+
 export default function FicheLogement() {
 
   const params = useParams() 
   console.log(params);
-
+  
   return (
     <div>
+
       <h1>voici la fiche Logement</h1> 
+      
       </div>
   )
 }
