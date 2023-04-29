4e1826929c37eb780f0cda5526a90554262f4b07
PA <pierreteychene@gmail.com>
2023-04-29 12:24:54 +0200

    carousel done

--- my-app/src/components/Carrousel/Carrousel.jsx
+++ my-app/src/components/Carrousel/Carrousel.jsx
@@ -1,3 +1,50 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+import vectorRight from '../../assets/images/VectorRight.svg'
+import vectorLeft from '../../assets/images/VectorLeft.png'
+const Carrousel = ({images}) => {
+    const [currentImage, setCurrentImage] = useState(0);
+    //Définir les fonctions suivant et précédent
+    const nextImage = () => {
+        let  newIndex  =  currentImage +1 >= images.length ? 0 :  currentImage +1
+        setCurrentImage(newIndex)
+    }
+     const previousImage = () => {
+        let  newIndex  =  currentImage -1 <= 0 ? images.length-1 :  currentImage -1
+        setCurrentImage(newIndex)
+    }
 
 
+    return(
+        <section className="containerCarrousel">
+        <div className="containerImageCarrousel">
+        <img className="imageCarrousel"
+            src={images[currentImage]}
+            alt={`${currentImage +1 }`}>
+        </img>
+        </div>
+        <div className="imageCounter">
+            {currentImage +1}/{images.length}
+        </div>
 
+        
+         <img className="leftArrow"
+            src={vectorLeft} 
+            alt="Précédent"
+            onClick={previousImage}>
+         </img>
+
+         <img className="rightArrow"
+            src={vectorRight} 
+            alt="Suivant"
+            onClick={nextImage}>
+         </img>
+        </section>
+    )
+}
+
+Carrousel.propTypes= {
+    images : PropTypes.array.isRequired
+}
+
+export default Carrousel
--- my-app/src/pages/FicheLogement/FicheLogement.jsx
+++ my-app/src/pages/FicheLogement/FicheLogement.jsx
@@ -1,17 +1,48 @@
 import {useParams}  from 'react-router-dom'
-
-
+import Carrousel from "../../components/Carrousel/Carrousel"
+import logements from '../../datas/logements'
+import Error from '../Error/Error'
+import Collaspe from '../../components/Collaspe/Collaspe'
 
 export default function FicheLogement() {
 
-  const params = useParams() 
-  console.log(params);
+  const {id} = useParams() 
+  const currentLogement = logements.find((logement) => logement.id === id)
+
+  console.log(currentLogement)
+
+  if(! currentLogement ){
+    return <Error />
+  }
+
+  //Récupérer les équipements
+  const equipements = currentLogement.equipments.map( (equipement) => (
+      <div key={equipement}>
+      {equipement}
+      <br/>
+    </div>
+  ))
+
   
   return (
     <div>
-
+      <Carrousel images={currentLogement.pictures} />
       <h1>voici la fiche Logement</h1> 
+      <div className='tagLogement'>
+        
+      </div>
       
+      <div className='containerCollaspe'>
+        <div className='containerDesciption'>
+          <Collaspe aboutTitle="Description" aboutText={currentLogement.description}></Collaspe>
+        </div>
+        <div className='containerEquipements'>
+                <Collaspe aboutTitle="Equipements" aboutText={equipements}></Collaspe>
+        </div>
+      
+
+      </div>
+
       </div>
   )
 }
